2dc2a9a6a043b19e5b424b29573d6d3e75617c1b
Pervez Mubeen <161222429+Mubeennaa12@users.noreply.github.com>
2026-09-25 21:57:29 +0530

    Time: 2 ms (77.17%), Space: 20.9 MB (29%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        count={}
+        for i in nums:
+            if i not in count:
+                count[i]=1
+            else:
+                count[i]+=1
+        for i in nums:
+            if count[i]==1:
+                return i
